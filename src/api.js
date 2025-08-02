0b44415be7591a34bbc92ebeabb939c194b65619
ADESPENZY <atoyebijoshua095@gmail.com>
2025-08-02 09:00:37 -0500

    Added Backend hosted url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { jwtDecode } from "jwt-decode";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL
