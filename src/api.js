00f295d88eb934dc4b0384b059075cc935e39af2
ADESPENZY <atoyebijoshua095@gmail.com>
2025-10-19 14:15:17 -0500

    Added new web service

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { jwtDecode } from "jwt-decode";
 
-const BASE_URL = "http://127.0.0.1:8000/";
+const BASE_URL = "https://outreach-dashboard-backend-jmkb.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL
